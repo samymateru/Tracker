66eaf0dcc9463fb4cf8b624237139456d5266257
Samwel Materu <samymateru1999@gmail.com>
2023-11-22 10:57:14 +0300

    Make app be able to fetch and update user details

--- src/components/account/account.jsx
+++ src/components/account/account.jsx
@@ -1,4 +1,5 @@
 import "./account.css"
+import {user, devices} from "../../state"
 function Account() {
     return ( 
         <div className="account-container">
@@ -8,20 +9,20 @@ function Account() {
                     <li className="account-info">
                         <div>
                             <div><span>Name:</span></div>
-                            <div><span>Samuel Materu</span></div>
+                            <div><span>{user.value.name}</span></div>
                         </div>
                         <div>
                             <div><span>Email:</span></div>
-                            <div><span>samymateru1999@gmail.com</span></div>
+                            <div><span>{user.value.email}</span></div>
                         </div>
                         <div>
                             <div><span>Tel:</span></div>
-                            <div><span>0787306314</span></div>
+                            <div><span>{(user.value.phone === null) ? "Not set": user.value.phone}</span></div>
                         </div>
                     </li>
                     <li>
                         <div><span>Devices:</span></div>
-                        <div><span>10</span></div>
+                        <div><span>{devices.value.length}</span></div>
                     </li>
                     <li>
                         <div><span>Notifications:</span></div>
@@ -29,7 +30,7 @@ function Account() {
                     </li>
                     <li>
                         <div><span>Status:</span></div>
-                        <div><span>Inactive</span></div>
+                        <div><span>{user.value.disabled ? "Inactive": "Active"}</span></div>
                     </li>
                 </ul>
             </div>
