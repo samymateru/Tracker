e07dae8e9c65c7438fa841a7cb063b58402b9c53
Samwel Materu <samymateru1999@gmail.com>
2023-11-29 16:03:14 +0300

    fixing bug

--- src/util/confirm/confirm.jsx
+++ src/util/confirm/confirm.jsx
@@ -14,10 +14,10 @@ const Confirm = (pros) => (
         <Dialog.Title className="delete-icon-dialogTitle">{pros.message}</Dialog.Title>
         <div className='contol-btn'>
           <Dialog.Close asChild>
-            <button className="delete-icon-okay" onClick={() => pros.onClick()}>OKAY</button>
+            <button className="delete-icon-cancel">CANCEL</button>
           </Dialog.Close>
           <Dialog.Close asChild>
-            <button className="delete-icon-cancel">CANCEL</button>
+            <button className="delete-icon-okay" onClick={() => pros.onClick()}>OKAY</button>
           </Dialog.Close>
         </div>
         <Dialog.Close asChild>
