52332fc1ffaebb253f211757703f33cf257de562
Samwel Materu <samymateru1999@gmail.com>
2023-11-29 16:00:20 +0300

    fixing

---
+++ src/util/confirm/confirm.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import './confirm.css';
+
+const Confirm = (pros) => (
+  <Dialog.Root>
+    <Dialog.Trigger asChild>
+      {pros.children}
+    </Dialog.Trigger>
+    <Dialog.Portal>
+      <Dialog.Overlay className="delete-icon-dialogOverlay" />
+      <Dialog.Content className="delete-icon-dialogContent">
+        <Dialog.Title className="delete-icon-dialogTitle">{pros.message}</Dialog.Title>
+        <div className='contol-btn'>
+          <Dialog.Close asChild>
+            <button className="delete-icon-okay" onClick={() => pros.onClick()}>OKAY</button>
+          </Dialog.Close>
+          <Dialog.Close asChild>
+            <button className="delete-icon-cancel">CANCEL</button>
+          </Dialog.Close>
+        </div>
+        <Dialog.Close asChild>
+          <button className="delete-device-icon" aria-label="Close">
+            <Cross2Icon />
+          </button>
+        </Dialog.Close>
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
+
+export default Confirm;
--- src/util/device/device.jsx
+++ src/util/device/device.jsx
@@ -1,6 +1,7 @@
 import { Cross1Icon } from "@radix-ui/react-icons";
 import { popups, positions, devices } from "../../state";
 import "./device.css"
+import Confirm from "../confirm/confirm";
 
 function Device() {
     async function delete_device(){
@@ -91,13 +92,15 @@ function Device() {
                          
               
             <div className="device-footer">
-                <button onClick={() => delete_device()}>
-                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4 7H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M6 7V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V7" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </button>
+                <Confirm message="Are you sure you want to delete this device" onClick={delete_device}>
+                    <button>
+                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M4 7H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M6 7V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V7" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                </Confirm>
                 <button>
                     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
