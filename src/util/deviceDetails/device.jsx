b4209894f3fb7f1e5494290812c7d9a9013b360a
Samwel Materu <samymateru1999@gmail.com>
2023-11-30 11:13:09 +0300

    test

--- src/util/deviceDetails/device.jsx
+++ src/util/deviceDetails/device.jsx
@@ -47,19 +47,19 @@ function deviceDetails() {
                             <div className="device-main">
                             <div className="device-data">
                                 <div><span>Speed</span></div>
-                                <div><span>{current_position.value.id}</span></div>
+                                <div><span>{current_position.value.speed || "None"}</span></div>
                             </div>
                             <div className="device-data">
                                 <div><span>Distance</span></div>
-                                <div><span>None</span></div>
+                                <div><span>{current_position.value.attributes.distance || "None"}</span></div>
                             </div>
                             <div className="device-data">
                                 <div><span>Ignition</span></div>
-                                <div><span>None</span></div>
+                                <div><span>{current_position.value.attributes.ignition ? "On" : "Off" || "None"}</span></div>
                             </div>
                             <div className="device-data">
                                 <div><span>Motion</span></div>
-                                <div><span>None</span></div>
+                                <div><span>{current_position.value.attributes.motion ? "On" : "Off" || "None"}</span></div>
                             </div>
                         </div> 
                         <div className="device-footer">
